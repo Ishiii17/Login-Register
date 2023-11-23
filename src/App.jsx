0d4c84cee37e2916a1313ad419fd02d611d9871b
Hiroshi Curimatmat <curimatmathiroshi0@gmail.com>
2023-11-23 13:36:51 +0800

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import LogInRegister from './Components/LogInRegister/LogInRegister';
+
+function App() {
+  return (
+    <div>
+      <LogInRegister/>
+    </div>
+  );
+}
+
+export default App;
